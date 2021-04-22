107a8ccbf023d708524ae1e02a7873613511bc58
Lindsay <lbarnett712@gmail.com>
2021-04-22 07:08:29 -0400

    feat: added dynamic products

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,10 @@ import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
-import products from '../products.json';
-import { fromImageToUrl } from '../utils/urls';
+import { fromImageToUrl, API_URL } from '../utils/urls';
 import { twoDecimals } from '../utils/format';
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -33,4 +32,17 @@ export default function Home() {
       ))}
     </div>
   );
+}
+
+export async function getStaticProps() {
+  // Fetch the products
+  const product_res = await fetch(`${API_URL}/products/`);
+  const products = await product_res.json();
+
+  // Return the products as props
+  return {
+    props: {
+      products,
+    },
+  };
 }
